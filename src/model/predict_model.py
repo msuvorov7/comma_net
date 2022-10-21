583f0636e20be428694e4a4925c54005a57e23b3
ruasvmv <maksim.suvorov@raiffeisen.ru>
2022-10-21 20:56:06 +0300

    refactoring for dvc workflow

--- src/model/predict_model.py
+++ src/model/predict_model.py
@@ -1,10 +1,20 @@
+import os
 import re
+import sys
 
 import torch
+from torch.utils.data import DataLoader
 
-from feature.build_dataloader import build_dataloader
-from feature.build_features import build_features, cut_text
-from feature.dataset import CommaDataset
+sys.path.insert(0, os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+))
+
+from src.feature.build_feature import build_features, ContentWrapper
+from src.feature.dataset import CommaDataset
+from src.model.train_model import collate_fn
+
+
+fileDir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../')
 
 
 decode_map = {0: '', 1: ',', 2: '.'}
@@ -28,27 +38,21 @@ def decode(words_original_case: list, y_predict: torch.Tensor, y_mask: torch.Ten
 
 def predict(path_to_text: str = 'predict_test.txt', batch_size: int = 2):
     with open(path_to_text) as file:
-        text = file.readlines()
+        text = file.read()
 
-    # print(text)
-    text = cut_text(100, text=text, is_train=False, return_text=True)
+    content = ContentWrapper(max_size=150).fit(text)
+    reshaped_text = content.get_split()
 
-    input_ids, input_targets, target_mask, attention_mask = build_features(
-        text=text,
-        return_features=True,
-        is_train=False,
-    )
-    # print(len(input_ids))
-    # print(input_ids, attention_mask)
+    input_ids, input_targets, target_mask, attention_mask = build_features(text)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     dataset = CommaDataset(input_ids, input_targets, target_mask, attention_mask)
-    test_dataloader = build_dataloader(dataset, batch_size=batch_size)
+    test_dataloader = DataLoader(dataset, batch_size=2, collate_fn=collate_fn, shuffle=False)
 
-    model = torch.load('models/model.pth')
+    # model = torch.load('models/model.pth')
+    model = torch.load('models/model.torch')
     model.to(device)
 
-    # print(text)
     result = ""
     with torch.no_grad():
         for i, batch in enumerate(test_dataloader):
@@ -61,12 +65,15 @@ def predict(path_to_text: str = 'predict_test.txt', batch_size: int = 2):
             y_predict = y_predict.view(-1, y_predict.shape[2])
             y_predict = torch.argmax(y_predict, dim=1).view(-1)
 
+            print(y_predict)
+
             string = ' '.join(list(
                 map(
                     lambda sent: 'SOS ' + sent + ' EOS',
-                    text[i * batch_size:i * batch_size + batch_size]
+                    reshaped_text[i * batch_size:i * batch_size + batch_size]
                 )
             ))
             words_original_case = list(filter(lambda word: word != ' ', re.split('(\W)', string)))
+            print(words_original_case)
             result += decode(words_original_case, y_predict, y_mask) + ' '
     print(result)
